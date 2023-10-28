605ec3d9f33f1e07115e9f9990c26d424bd12581
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-10-28 21:44:36 +0530

    🚀 v1.7

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const userRouter = express.Router()
-const {getLandingPage , login, signup} = require('../controller/index.js')
+const {getLandingPage , login, signup, sendLoginFile, sendSignUpFile} = require('../controller/index.js')
 userRouter.get('/landingpage', getLandingPage)
+// .get('/loginform', sendLoginFile)
+// .get('/signupform', sendSignUpFile)
 .post('/signup', signup)
+.post('/login', login)
 
 module.exports= userRouter
