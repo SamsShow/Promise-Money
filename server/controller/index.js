16e2122f81260ff3934e6148725aaf00cc0a8f71
Naveen <navyn13102003@gmail.com>
2023-10-28 17:38:24 +0530

    login and signup'

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,6 +1,48 @@
 const path = require('path')
+const bcrypt = require('bcrypt')
 
 exports.getLandingPage = (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, "../../interface/index.html" ));
+    return res.status(200).sendFile(path.join(__dirname, "../../interface/index.html" ));
+}
+exports.signup = (req, res)=>{
+  console.log(req.body)
+  const { name, email, phone, DOB, address, password} = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const user={
+    name,
+    email,
+    phone,
+    DOB,
+    address,
+    hashedPassword
+  }
+  console.log(user)
+  console.log(hashedPassword)
+}
 
-  };
+// exports.signup = async (req, res) => {
+//   const { username, email, password } = req.body;
+//   const hashedPassword = bcrypt.hashSync(password, 10);
+//   if (!username || !email || !password) {
+//     return res.status(400).send("Please fill in all fields.");
+//   }
+//   try {
+
+//     await client.connect();
+//     console.log("Successfully connected to mongodb atlas");
+//     const db = client.db("Cluster0");
+//     const collection = db.collection("users");
+//     const user = {
+//       username,
+//       email,
+//       hashedPassword,
+//     };
+//     console.log(user);
+//     const result = await collection.insertOne(user);
+//     console.log("Result got saved with id_" + result.insertedId);
+//     await client.close();
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   res.redirect("/loginform");
+// };
